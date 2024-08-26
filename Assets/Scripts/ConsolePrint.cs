0705ff7cd9859366a9b5c5b35d7d7245115effa3
norman <wswblack@gmail.com>
2024-08-26 20:17:01 +1000

    80% complete

---
+++ Assets/Scripts/ConsolePrint.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+    int i;
+    int random;
+    public Renderer read;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(gameObject.name + ":" + ++i);
+
+    }
+}
