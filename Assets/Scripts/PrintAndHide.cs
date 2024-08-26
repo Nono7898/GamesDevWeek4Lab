07b427b074babdda286ceceb917e88b58a5194c9
norman <wswblack@gmail.com>
2024-08-26 20:24:24 +1000

    90

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,7 +6,7 @@ public class ConsolePrint : MonoBehaviour
 {
     int i;
     int random;
-    public Renderer read;
+    public Renderer rend;
 
     // Start is called before the first frame update
     void Start()
